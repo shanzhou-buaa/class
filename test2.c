ff9536b1e4d30421bd62a37a447092028367894e
shanzhou-buaa <2115362170@qq.com>
2021-01-16 08:13:57 +0800

    上课代码

---
+++ test2.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <assert.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+int test = 2021;
+int Add(int x, int y)
+{
+	return x + y;
+}
+//int main()    //去掉注释加上就错了
+//{
+//	
+//    return 0;
+//}
